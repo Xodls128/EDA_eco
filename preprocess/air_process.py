9153afee748ac97672649f110cfa4d7b7f7b4b68
xodls <pider6@naver.com>
2025-05-12 12:41:19 +0900

    대기오염 전처리:숫자 자리에 문자열들어있는경우 제외

--- preprocess/air_process.py
+++ preprocess/air_process.py
@@ -15,7 +15,8 @@ class AirQualityProcessor:
         pm10_cols = [col for idx, col in enumerate(self.df.columns[1:]) if idx % 2 == 0]
         pm25_cols = [col for idx, col in enumerate(self.df.columns[1:]) if idx % 2 == 1]
 
-        self.df["PM10_avg"] = self.df[pm10_cols].astype(float).mean(axis=1, skipna=True)
-        self.df["PM25_avg"] = self.df[pm25_cols].astype(float).mean(axis=1, skipna=True)
+        # 문자열 값이 있을 수 있으므로 float 변환 시 에러 무시하고 NaN 처리
+        self.df["PM10_avg"] = self.df[pm10_cols].apply(pd.to_numeric, errors='coerce').mean(axis=1)
+        self.df["PM25_avg"] = self.df[pm25_cols].apply(pd.to_numeric, errors='coerce').mean(axis=1)
 
         return self.df[["자치구", "PM10_avg", "PM25_avg"]]
